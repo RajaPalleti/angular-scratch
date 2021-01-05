bebad4fc15bc15e06521f46c3b435d298cecd0de
RajaPalleti <54736267+RajaPalleti@users.noreply.github.com>
2021-01-05 21:45:17 +0530

    Add files via upload

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { usersList } from './users-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      let credentials = JSON.parse(localStorage.getItem('userDetails'));
+      console.log('credentials', credentials);
+      if(credentials['userName'] == null && credentials.password == null) {
+        this.router.navigate(['login']);
+        return false;
+      } else {
+        return true;
+      }
+  }
+}
